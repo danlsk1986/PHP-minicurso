55e568378bf855c99a26fdadddb44ff6abbd1c1e
dan lasauskas <danlsk1986@gmail.com>
2020-06-14 22:00:14 -0300

    Arreglos Asociativos / tipo Obj

---
+++ clase_12/arrayAsociativo.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Aprendiendo PHP</title>
+    <link href="https://fonts.googleapis.com/css?family=Proza+Libre" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/estilos.css" media="screen" title="no title">
+  </head>
+  <body>
+
+    <div class="contenedor">
+      <h1>Aprendiendo PHP</h1>
+
+        <div class="contenido">
+            <?php
+              $persona = array(
+                'nombre'=> 'Juan',
+                'pais'=> 'Mexico',
+                'profesion'=> 'Desarrollador web'
+              );
+           ?>
+
+           <pre>
+                <?php print_r($persona)?>
+           </pre>
+
+            <!-- Se accede por llave no por indice -->
+           <?php echo $persona['profesion'];  ?>
+          
+           <pre>
+           <!-- cambiando de asociativo a indexado -->
+                <?php print_r(array_values($persona))?>
+                <?php print_r(array_keys($persona))?>
+           </pre>
+
+
+
+
+
+
+            
+            
+          
+        </div>
+    </div>
+
+
+
+
+  </body>
+</html>
